6ebdea15216f2536c78f8b31cfc8a8665c136a92
dfinger11 <the11thfinger11@gmail.com>
2019-05-25 15:42:17 -0700

    hopefully fixed login bug

--- web/project/loginForm.php
+++ web/project/loginForm.php
@@ -6,14 +6,47 @@ $password = crypt(htmlspecialchars($_POST['password']), CRYPT_BLOWFISH);
 $db = get_db();
 $statement = $db->prepare("SELECT * FROM famusers WHERE username='$username' AND password = '$password'");
 $userFound = $statement->execute();
-
-if($userFound) {
-    $_SESSION['authenticated'] = true;
-    header("Location:familyHome.php");
-} else {
-    $_SESSION['authenticated'] = false;
-    header("Location:login.php");
-    ?><span><?php echo "Login failed!"?><span><?php
-}
 ?>
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'/>
+    <meta name='viewport'
+    <title>Family Game Plan Login</title>
+    <link rel='stylesheet' href=''/>
+    <script src=""></script>
+</head>
+<body>
+<div class="page">
+    <div class="header">
+        <h1 class="textHeader1">The Family Game Plan</h1>
+        <h2 class="textHeader3">Designed to help you manage your family projects</h2>
+    </div>
+    <div class="content">
+        <h3>Login</h3>
+        <form action="loginForm.php" method="post">
+            Username: <input type="text" name="username">
+            <br>
+            Password: <input type="password" name="password">
+            <br>
+            <input type="submit" value="Login">
+        </form>
+        <?php
+
+        if($userFound) {
+            $_SESSION['authenticated'] = true;
+            header("Location:familyHome.php");
+        } else {
+        $_SESSION['authenticated'] = false;
+        header("Location:login.php");
+        ?><span><?php echo "Login failed!"?><span><?php
+                }
+                ?>
+        <h3>Don't have an account <a  onclick="location.href = 'register.php';" style="color: blue">click here</a>!</h3>
+    </div>
+    <div class="footer">
+
+    </div>
+</div>
+</body>
 
