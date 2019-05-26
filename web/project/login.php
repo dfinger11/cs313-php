9354122121366c524094c9aa11378a21dda808f9
dfinger11 <the11thfinger11@gmail.com>
2019-05-25 18:51:46 -0700

    continue work on login

--- web/project/login.php
+++ web/project/login.php
@@ -5,6 +5,22 @@ if(isset($_SESSION['authenticated'])) {
         header("Location:familyHome.php");
     }
 }
+require "../../database/dbConnect.php";
+function authenticate()
+{
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    $db = get_db();
+    $statement = $db->prepare("SELECT * FROM famusers WHERE username='$username' AND password_hash = '$password'");
+    $statement->execute();
+
+    $rowCount = $statement->rowCount();
+    if (!empty($rowCount) && $rowCount == 1) {
+        $_SESSION['authenticated'] = true;
+    } else {
+        $_SESSION['authenticated'] = false;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang='en'>
@@ -23,13 +39,21 @@ if(isset($_SESSION['authenticated'])) {
     </div>
     <div class="content">
         <h3>Login</h3>
-        <form action="formLogin.php" method="post">
+        <form action="login.php" method="post">
             Username: <input type="text" name="username">
             <br>
             Password: <input type="password" name="password">
             <br>
             <input type="submit" value="Login">
         </form>
+        <?php
+        if($_SESSION['authenticated'] == true) {
+            ?><span><?php echo "Login success!"?><span><?php
+        } else {
+            header("Location:login.php");
+            ?><span><?php echo "Login failed!"?><span><?php
+        }
+        ?>
         <h3>Don't have an account <a  onclick="location.href = 'register.php';" style="color: blue">click here</a>!</h3>
     </div>
     <div class="footer">
