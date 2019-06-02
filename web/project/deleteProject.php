a4cd82ed6db1fa6ce69baf4ac21cc434e5906a9f
dfinger11 <the11thfinger11@gmail.com>
2019-06-01 19:49:47 -0700

    updating family page to add projects

--- web/project/deleteProject.php
+++ web/project/deleteProject.php
@@ -2,11 +2,12 @@
 session_start();
 require "../../database/dbConnect.php";
 $project = $_POST['deadProject'];
+$username = $_SESSION['username'];
 $checkStatement = get_db()->prepare("SELECT project_name FROM project WHERE project_name='$project';");
 $checkStatement->execute();
 $checkRows = $checkStatement->rowCount();
 if(!empty($checkRows) && $checkRows > 0) {
-    $deleteStatement = get_db()->prepare("DELETE FROM project WHERE project_name='$project';");
+    $deleteStatement = get_db()->prepare("DELETE FROM project WHERE project_name='$project' AND family_fk=(SELECT family_fk FROM famusers WHERE username='$username';");
     $deleteStatement->execute();
 }
 header("Location: familyHome.php");
--- web/project/deleteTask.php
+++ web/project/deleteTask.php
@@ -2,11 +2,12 @@
 session_start();
 require "../../database/dbConnect.php";
 $task = $_POST['deadTask'];
+$project = $_SESSION['project'];
 $checkStatement = get_db()->prepare("SELECT task_title FROM task WHERE task_title='$task';");
 $checkStatement->execute();
 $checkRows = $checkStatement->rowCount();
 if(!empty($checkRows) && $checkRows > 0) {
-    $deleteStatement = get_db()->prepare("DELETE FROM task WHERE task_title='$task';");
+    $deleteStatement = get_db()->prepare("DELETE FROM task WHERE task_title='$task' AND project_fk='$project';");
     $deleteStatement->execute();
 }
 header("Location: projectView.php");
