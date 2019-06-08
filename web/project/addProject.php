17f195fbf227ccf0bee8b6498c232df3c6290fcf
dfinger11 <the11thfinger11@gmail.com>
2019-06-08 15:50:16 -0700

    adding css to the project

--- web/project/addProject.php
+++ web/project/addProject.php
@@ -34,14 +34,16 @@ if(isset($_SESSION['authenticated']) && $_SESSION['authenticated'] == true) {
     <div class="section header">
         <h1>Create Project</h1>
     </div>
-    <div class="section content centerContent">
-        <form action="addProjectForm.php" method="post">
-            <span style="color: red">*</span>Project Name: <input type="text" name="projectName">
-            <br>
-            Project Deadline: Month <input type="number" maxlength="2"  name="month">, Day <input type="number" maxlength="2"  name="day">, Year <input type="number" maxlength="4"  name="year">
-            <br>
-            <input class="button" type="submit">
-        </form>
+    <div class="section content">
+        <div class="centerContent">
+            <form action="addProjectForm.php" method="post">
+                <span style="color: red">*</span>Project Name: <input type="text" name="projectName">
+                <br>
+                Project Deadline: Month <input type="number" maxlength="2"  name="month">, Day <input type="number" maxlength="2"  name="day">, Year <input type="number" maxlength="4"  name="year">
+                <br>
+                <input class="button" type="submit">
+            </form>
+        </div>
     </div>
     <div class="section footer">
         <p class="footerClass">Derek Finger 2019</p>
