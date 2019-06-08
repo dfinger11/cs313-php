d96c4cdd52d9f5fe5627df3c7459b3b96fb9673f
dfinger11 <the11thfinger11@gmail.com>
2019-06-08 16:56:34 -0700

    adding css to the project

--- web/project/deleteTask.php
+++ web/project/deleteTask.php
@@ -7,7 +7,7 @@ $checkStatement = get_db()->prepare("SELECT task_title FROM task WHERE task_titl
 $checkStatement->execute();
 $checkRows = $checkStatement->rowCount();
 if(!empty($checkRows) && $checkRows > 0) {
-    $deleteStatement = get_db()->prepare("DELETE FROM task WHERE task_title='$task' AND project_fk='$project';");
+    $deleteStatement = get_db()->prepare("DELETE FROM task WHERE task_title='test' AND project_fk=(SELECT project_pk FROM project WHERE family_fk=(SELECT family_fk FROM famusers WHERE username='$username') AND project_name='$project');");
     $deleteStatement->execute();
 }
 header("Location: projectView.php");
