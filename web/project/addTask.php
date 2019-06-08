967a9bac16d98a686c92b330b9e284f98ab7ef67
dfinger11 <the11thfinger11@gmail.com>
2019-06-08 16:32:04 -0700

    adding css to the project

--- web/project/addTask.php
+++ web/project/addTask.php
@@ -73,6 +73,7 @@ if(isset($_SESSION['authenticated']) && $_SESSION['authenticated'] == true) {
                 </select>
                 <br>
                 <input class="button" type="submit">
+                <br>
             </form>
         </div>
     </div>
